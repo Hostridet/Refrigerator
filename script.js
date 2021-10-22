936d1eb225ffcb8450b822b4983563a4e2f5db84
Никита Румянцев <rumiantcev.no@students.dvfu.ru>
2021-10-22 23:38:12 +1000

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const main = document.querySelector(".main");
+
+function create_field()
+{
+    let field = document.createElement("div");
+    field.setAttribute("class", "field");
+    main.appendChild(field);
+}
+function Exchange(n)
+{
+    for(let i = 0; i < 5; i++)
+    {
+        a
+    }
+}
